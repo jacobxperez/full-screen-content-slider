522a938f8403bc0f224eeaf371cf20dda01cf6e8
Jacob <jacobxperez@gmx.com>
2023-03-29 21:34:03 -0600

    Combine the cycleItems and changeSlide functions into one function to simplify the code

--- js/slider.js
+++ js/slider.js
@@ -4,94 +4,90 @@
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
 ------------------------------------------------------------------------------*/
-const sliders = document.querySelectorAll('.slider')
+const sliders = document.querySelectorAll('.slider');
 
 const slider = () => {
     sliders.forEach((slider) => {
-        const slides = slider.querySelectorAll('.slide')
-        const totalSlides = slides.length
-        const imgCache = []
-        let currIndex = 0
-        let intervalTime = 5000
-        let sliderInterval
+        const slides = slider.querySelectorAll('.slide');
+        const totalSlides = slides.length;
+        const imgCache = [];
+        let currIndex = 0;
+        let intervalTime = 5000;
+        let sliderInterval;
 
         const cycleItems = () => {
-            const currSlide = slides[currIndex]
+            const currSlide = slides[currIndex];
 
             slides.forEach((slide) => {
-                slide.style.visibility = 'hidden'
-                slide.style.zIndex = 1
-                slide.style.opacity = 0 // added for fade effect
-            })
+                slide.style.visibility = 'hidden';
+                slide.style.zIndex = 1;
+                slide.style.opacity = 0;
+            });
 
-            currSlide.style.visibility = 'visible'
-            currSlide.style.zIndex = 5
-            currSlide.style.opacity = 1 // added for fade effect
-            currSlide.style.transition = 'all 0.5s ease-in-out 0s' // added for fade effect
-        }
+            currSlide.style.visibility = 'visible';
+            currSlide.style.zIndex = 5;
+            currSlide.style.opacity = 1;
+            currSlide.style.transition = 'all 0.5s ease-in-out 0s';
+        };
 
-        const changeSlide = () => {
-            currIndex += 1
-
-            if (currIndex > totalSlides - 1) {
-                currIndex = 0
+        const changeSlide = (direction) => {
+            if (direction === 'next') {
+                currIndex += 1;
+                if (currIndex > totalSlides - 1) {
+                    currIndex = 0;
+                }
+            } else if (direction === 'prev') {
+                currIndex -= 1;
+                if (currIndex < 0) {
+                    currIndex = totalSlides - 1;
+                }
             }
 
-            cycleItems()
-        }
+            cycleItems();
+        };
 
-        const startSlider = () => {
-            clearInterval(sliderInterval)
+        const startSlider = (time) => {
+            clearInterval(sliderInterval);
+
+            intervalTime = time || intervalTime;
 
             sliderInterval = setInterval(() => {
-                changeSlide()
-            }, intervalTime)
-        }
+                changeSlide('next');
+            }, intervalTime);
+        };
 
         const preloadImages = () => {
             for (const slide of slides) {
-                const imgSrc = slide.querySelector('img').src
+                const imgSrc = slide.querySelector('img').src;
                 const imgPromise = new Promise((resolve) => {
-                    const img = new Image()
-                    img.src = imgSrc
-                    img.onload = resolve
-                })
-                imgCache.push(imgPromise)
+                    const img = new Image();
+                    img.src = imgSrc;
+                    img.onload = resolve;
+                });
+                imgCache.push(imgPromise);
             }
-            return Promise.all(imgCache)
-        }
+            return Promise.all(imgCache);
+        };
 
         preloadImages().then(() => {
-            cycleItems()
-            startSlider()
-        })
+            cycleItems();
+            startSlider();
+        });
 
-        const nextSlideBtn = slider.querySelector('.next-slide')
+        const nextSlideBtn = slider.querySelector('.next-slide');
 
         nextSlideBtn.addEventListener('click', () => {
-            currIndex += 1
+            changeSlide('next');
+            startSlider(8000);
+        });
 
-            if (currIndex > totalSlides - 1) {
-                currIndex = 0
-            }
-
-            cycleItems()
-            startSlider((intervalTime = 8000))
-        })
-
-        const prevSlideBtn = slider.querySelector('.prev-slide')
+        const prevSlideBtn = slider.querySelector('.prev-slide');
 
         prevSlideBtn.addEventListener('click', () => {
-            currIndex -= 1
+            changeSlide('prev');
+            startSlider(8000);
+        });
+    });
+};
 
-            if (currIndex < 0) {
-                currIndex = totalSlides - 1
-            }
-
-            cycleItems()
-            startSlider((intervalTime = 8000))
-        })
-    })
-}
-
-slider()
+slider();
